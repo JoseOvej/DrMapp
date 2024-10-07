ef22e679af1f2f58bf2e0eee0b71e1a9ac48d697
JoseOvej <jiovejero2012@gmail.com>
2024-10-07 12:04:08 -0300

    Update Salud_a_la_mano_App.py

--- Salud_a_la_mano_App.py
+++ Salud_a_la_mano_App.py
@@ -92,7 +92,7 @@ def encontrar_localizaciones_cercanas(latitud_referencia, longitud_referencia, d
     distancias_df = pd.DataFrame(distancias)
 
     if distancias_df.empty:
-        st.warning("No hay localizaciones dentro del rango especificado. Seleccione otro servicio, o reinicie la búsqueda.")
+        st.warning("No hay localizaciones dentro del rango especificado. Seleccione uno o más servicios, o reinicie la búsqueda.")
         st.stop()
 
     # Ordenar el DataFrame por la distancia en orden ascendente
